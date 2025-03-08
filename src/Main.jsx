29c806af6d246a18ecc5cfb513a0c9bb1a485c03
geonhwi05 <hyem92.11.07@gmail.com>
2025-03-09 01:43:02 +0900

    redux

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,16 +3,23 @@ import styled from 'styled-components';
 // import DetailModal from './components/DetailModal';
 import DataList from './components/DataList';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setIsLoading, setIsMoreItems, setItems } from './redux/modules/data';
 
 const InfiniteScroll = () => {
-  const [items, setItems] = useState([]); // 데이터 목록
-  // const [selectedPost, setSelectedPost] = useState(null); // 선택 게시글
-  const [isMoreItems, setIsMoreItems] = useState(true); // 데이터 끝 확인
-  const [isLoading, setIsLoading] = useState(false); // 로딩 스피너
+  // const [items, setItems] = useState([]); // 데이터 목록
+  // const [isMoreItems, setIsMoreItems] = useState(true); // 데이터 끝 확인
+  // const [isLoading, setIsLoading] = useState(false); // 로딩 스피너
   // const [isModal, setIsModal] = useState(false); //모달 관리
+  // const [selectedPost, setSelectedPost] = useState(null); // 선택 게시글
 
   const pageRef = useRef(1); // 현재 페이지 추적(리렌더링 방지)
   const loaderRef = useRef(null); // 스크롤 로더 감지 참조
+
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.data.items);
+  const isMoreItems = useSelector((state) => state.data.isMoreItems);
+  const isLoading = useSelector((state) => state.data.isLoading);
 
   // const onClickModal = (selectedPost) => {
   //   setSelectedPost(selectedPost);
@@ -26,40 +33,43 @@ const InfiniteScroll = () => {
   };
 
   const fetchData = async () => {
-    setIsLoading(true) // 스피너
+    // 더 이상 데이터가 없다면 데이터를 더 이상 로드하지 않도록 설정
+    if (!isMoreItems || isLoading) return;
+  
+    dispatch(setIsLoading(true));
+  
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageRef.current}`);
     const newData = await res.json();
-
+  
     if (newData.length === 0) {
-      setIsMoreItems(false);
-    } // 데이터 다 보여줬으면 스피너 종료
-
-    setItems((prev) => [...prev, ...newData]); // 기존 데이터에 추가
+      dispatch(setIsMoreItems(false)); 
+    }
+  
+    dispatch(setItems(newData));
     pageRef.current += 1; // 페이지 증가
-    setIsLoading(false);
+    dispatch(setIsLoading(false));
   };
-
+  
   // Intersection Observer 사용해 스크롤 감지, 특정요소가 뷰포트에 얼마나 보이는지를 감지함
   // 화면에 loader가 보이면 fetch data 실행해서 새로운 데이터 보여줌
   useEffect(() => {
+    if (isLoading) return;  // 로딩 중이면 observer를 설정하지 않음
 
     const observer = new IntersectionObserver(
-      ([entry]) => { // 감시중인 요소들의 배열
-        if (entry.isIntersecting && !isLoading) {
-          fetchData();
-        } // 관찰 대상이 화면에 보이고 로딩상태가 아니라면 데이터 보여줌
+      ([entry]) => {
+        if (entry.isIntersecting && !isLoading && isMoreItems) {
+          fetchData();  // 새로운 데이터를 로드
+        }
       },
-      { threshold: 1.0 } // 해당요소 100% 보여야 동작
+      { threshold: 1.0 }
     );
 
     observer.observe(loaderRef.current);
-    // useRef는 .current를 통해 접근해야 DOM 요소를 얻을 수 있음
 
     return () => {
       observer.disconnect();
-    }; // 클린업 함수
-  }, [isLoading]);
-
+    };
+  }, [isLoading, isMoreItems]);
 
   return (
 
@@ -94,7 +104,6 @@ const InfiniteScroll = () => {
 export default InfiniteScroll;
 
 const MainWrap = styled.div`
-  width: 1620px;
   margin: 0 auto;
 
   h1{
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,20 +1,24 @@
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { setPost } from "../redux/modules/data";
 
 const DetailPage = () => {
   const { id } = useParams();
-  const [post, setPost] = useState(null);
+  // const [post, setPost] = useState(null);
+  const dispatch = useDispatch();
+  const post = useSelector((state) => state.data.post);
 
   useEffect(() => {
     const fetchPost = async () => {
       const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-      const data = await res.json();
-      setPost(data);
+      const selectData = await res.json();
+      dispatch(setPost(selectData));
     };
 
     fetchPost();
-  }, [id]);
+  }, [dispatch, id]);
 
 
   return (
